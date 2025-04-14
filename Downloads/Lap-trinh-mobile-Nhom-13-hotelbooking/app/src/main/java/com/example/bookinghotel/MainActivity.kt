fea6f331b17e0985cc5302ac60f04621cf7f31ee
PTriet <nguyentriet0112@gmail.com>
2025-04-14 18:03:07 +0700

    Update hotelbooking

---
+++ Downloads/Lap-trinh-mobile-Nhom-13-hotelbooking/app/src/main/java/com/example/bookinghotel/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.bookinghotel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+import com.example.bookinghotel.ui.theme.BookingHotelTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BookingHotelTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HotelBookingScreen( // Gọi giao diện từ file hotelbooking.kt
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
